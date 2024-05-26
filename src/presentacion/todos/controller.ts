7bcdc657c55ae8d1fc8c415ecc76d95a985423cb
Emerson Roncancio <roncancioavila@gmail.com>
2024-05-26 01:42:07 -0500

    CRUD con prsima y postrgres

--- src/presentacion/todos/controller.ts
+++ src/presentacion/todos/controller.ts
@@ -1,107 +1,98 @@
 import { Request, Response } from 'express'
 import { todo } from 'node:test'
-
-interface typeTodos {
-    id: number,
-    nombre: string,
-    apellido: string,
-    edad: number
-}
-
-const todos: typeTodos[] = [
-    {
-        "id": 1,
-        "nombre": "Emerson",
-        "apellido": "Roncancio",
-        "edad": 19
-    },
-    {
-        "id": 2,
-        "nombre": "Deimer",
-        "apellido": "Roncancio",
-        "edad": 21
-    },
-    {
-        "id": 3,
-        "nombre": "Daniela",
-        "apellido": "Perez",
-        "edad": 18
-    },
-]
+import { prisma } from '../../data/postgres'
 
 export class TodosController {
 
     constructor() { }
 
-    public getTodos = (req: Request, res: Response) => {
-        res.json(todos)
-    }
+    public getTodos = async (req: Request, res: Response) => {
 
-    public getTodoId = (req: Request, res: Response) => {
-        const { id } = req.params
-
-        if (isNaN(parseInt(id))) return res.status(400).json({ mensaje: "Argumento id no es un numero" })
-        const todo = todos.filter((todo) => todo.id === parseInt(id))
-        if (todo.length === 0) return res.status(404).json({ mensaje: "Todo no existe" })
+        const todo = await prisma.usuarios.findMany()
 
         res.json(todo)
     }
 
-    public createTodo = (req: Request, res: Response) => {
+    public getTodoId = async (req: Request, res: Response) => {
+        const { id } = req.params
+
+        if (isNaN(parseInt(id))) return res.status(404).json({ mensaje: "Argumento id no es un numero" })
+        // const todo = todos.filter((todo) => todo.id === parseInt(id))
+        const todo = await prisma.usuarios.findFirst({
+            where: {
+                id: parseInt(id)
+            }
+        })
+
+        if (!todo) return res.status(404).json({ mensaje: "Todo no existe" })
+
+        res.json(todo)
+    }
+
+    public createTodo = async (req: Request, res: Response) => {
         const body = req.body
         const { nombre, apellido, edad } = body
 
         if (!nombre || !apellido || !edad) return res.status(400).json({ messageError: "No se estan enviando los argumentos requeridos" })
 
-        const newTodoId = todos.slice(-1)[0]
+        const todo = await prisma.usuarios.create({
+            data: {
+                nombre,
+                apellido,
+                edad
+            }
+        })
 
-        const newId = newTodoId.id + 1
-
-        const newTodo: typeTodos = {
-            id: newId,
-            nombre,
-            apellido,
-            edad
-        }
-
-        todos.push(newTodo)
-
-        res.json(newTodo)
+        res.json(todo)
     }
 
-    public UpdateTodo = (req: Request, res: Response) => {
+    public UpdateTodo = async (req: Request, res: Response) => {
         const { id } = req.params
-        if (isNaN(parseInt(id))) return res.status(400).json({ mensaje: "Argumento id no es un numero" })
+        if (isNaN(parseInt(id))) return res.status(404).json({ mensaje: "Argumento id no es un numero" })
         const body = req.body
         const { nombre, apellido, edad } = body
         if (!nombre || !apellido || !edad) return res.status(400).json({ messageError: "No se estan enviando los argumentos requeridos" })
-        const todoTest = todos.find(todo => todo.id == parseInt(id))
-        if (!todoTest) return res.status(400).json({ messageError: "la id del elmento no existe" })
 
-        todos.forEach((todo) => {
-            if (todo.id === parseInt(id)) {
-                todo.nombre = nombre
-                todo.apellido = apellido
-                todo.edad = edad
+        const todoNotExist = await prisma.usuarios.findFirst({
+            where: {
+                id: parseInt(id)
             }
         })
 
-        res.json(todos)
+        if (!todoNotExist) return res.status(404).json({ messageError: "la id del elmento no existe" })
+
+        const todo = await prisma.usuarios.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                nombre,
+                apellido,
+                edad
+            }
+        })
+
+        res.json(todo)
     }
 
-    public DeleteTodo = (req: Request, res: Response) => {
+    public DeleteTodo = async (req: Request, res: Response) => {
         const { id } = req.params
         if (isNaN(parseInt(id))) return res.status(400).json({ mensaje: "Argumento id no es un numero" })
 
-        const todoTest = todos.find(todo => todo.id == parseInt(id))
-        if (!todoTest) return res.status(400).json({ messageError: "la id del elmento no existe" })
-
-        todos.forEach((todonew, index) => {
-            if (todonew.id === parseInt(id)) {
-                todos.splice(index, 1)
+        const todoNotExist = await prisma.usuarios.findFirst({
+            where: {
+                id: parseInt(id)
             }
         })
 
-        res.json(todos)
+        if (!todoNotExist) return res.status(400).json({ messageError: "la id del elmento no existe" })
+
+        const todo = await prisma.usuarios.delete({
+            where: {
+                id: parseInt(id)
+            }
+        })
+
+        res.json(todo)
     }
 }
